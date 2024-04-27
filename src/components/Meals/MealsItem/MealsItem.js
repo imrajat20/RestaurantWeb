9aa090e8b73ab1dde22190400dc29f9e911abb36
imrajat20 <yaduvanshiamit3@gmail.com>
2024-04-27 21:55:02 +0530

    Form

--- src/components/Meals/MealsItem/MealsItem.js
+++ src/components/Meals/MealsItem/MealsItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './MealsItem.module.css';
+import MealItemForm from "./MealItenForm";
 
 const MealsItem = props => {
     const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +11,9 @@ const MealsItem = props => {
             <div className={classes.description}>{props.description}</div>
             <div className={classes.price}>{price}</div>
         </div>
-        <div></div>
+        <div>
+            <MealItemForm></MealItemForm>
+        </div>
     </li>
 };
 
